2547082e918c9df4a932347123f9408b48f69a4c
BreakZhao <1694392889@qq.com>
2022-06-14 11:36:50 +0800

    feat: Init The SpringBoot Server
    set test name
    
    About #1

--- server/src/test/java/org/adp/gable/GableApplicationTests.java
+++ server/src/test/java/org/adp/gable/GableApplicationTests.java
@@ -1,6 +1,7 @@
 package org.adp.gable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,6 +16,7 @@ class GableApplicationTests {
 	ObjectMapper objectMapper;
 
 	@Test
+	@DisplayName("test app start")
 	void contextLoads() {
 		assertNotNull(objectMapper);
 	}
