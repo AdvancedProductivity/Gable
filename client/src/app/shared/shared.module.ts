809e4196bc6fe29f336ee6701e7f1ea90197e69d
zzq <1694392889@qq.com>
2022-06-23 22:26:04 +0800

    feat: add material design ui component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -6,10 +6,16 @@ import { TranslateModule } from '@ngx-translate/core';
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
+import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
   imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  exports: [
+    TranslateModule
+    , WebviewDirective
+    , FormsModule
+    , MatSliderModule,
+  ]
 })
 export class SharedModule {}
