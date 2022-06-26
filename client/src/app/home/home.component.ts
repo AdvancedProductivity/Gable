faf1797756336c8fa0b6bf7259b2e9655a3885db
BreakZhao <1694392889@qq.com>
2022-06-26 17:52:46 +0800

    feat: init the login component
    about #9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {DataServiceImplService} from '../core/services/data-service-impl.service';
-
+import '../../assets/login-animation.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit {
   editorOptions = {theme: 'vs-dark', language: 'javascript'};
   code = 'function x() {\nconsole.log("Hello world!");\n}';
+  email: string;
+  password: string;
 
   constructor(private router: Router,
               private dataServiceImplService: DataServiceImplService) {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    (window as any).loginInitialize();
   }
 
   addItem() {
@@ -28,5 +34,11 @@ export class HomeComponent implements OnInit {
     this.dataServiceImplService.clearAll().subscribe((v) => {
       console.log('zzq see clear data', v);
     });
+  }
+
+  login() {
+
+    console.log(`email: ${this.email} password: ${this.password}`)
+    alert(`Email: ${this.email} Password: ${this.password}`)
   }
 }
