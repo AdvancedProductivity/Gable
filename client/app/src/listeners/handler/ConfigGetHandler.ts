cdc07171d52a18d0a6222128bfa0e59c539f523b
zzq <1694392889@qq.com>
2022-07-10 18:24:44 +0800

    fix: create or update error
    about #15

--- client/app/src/listeners/handler/ConfigGetHandler.ts
+++ client/app/src/listeners/handler/ConfigGetHandler.ts
@@ -1,5 +1,6 @@
 import {Handler} from "../listener-handler";
 import {getConfigRepository, getItemRepository} from "../../config/init-datasource";
+import {Config} from "../../entity/Config";
 
 export class ConfigGetHandler implements Handler {
 
@@ -24,9 +25,25 @@ export class ConfigSetHandler implements Handler {
 
   async handle(args: any[]) {
     if (args[0] && args[1]) {
-      const config = {key: args[0], value: args[1]}
       const configRepository = await getConfigRepository();
-      await configRepository.save(config)
+      let config = await configRepository.findOne({
+        where: {
+          key: args[0]
+        }
+      });
+      if (!config) {
+        console.log('create new config')
+        config = {key: args[0], value: args[1]}
+        await configRepository.save(config)
+      }else {
+        console.log('update old config')
+        config.value = args[1];
+        await configRepository
+          .createQueryBuilder('config')
+          .update<Config>(Config, {value: args[1]})
+          .where("config.key = :key", {key: args[0]})
+          .execute();
+      }
       return 'success';
     }
     return 'failed';
