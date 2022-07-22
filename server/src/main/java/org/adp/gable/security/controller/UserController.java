e21e269726f8e520bad1e413d52fd5d1a786af47
zzq <1694392889@qq.com>
2022-07-22 23:53:53 +0800

    feat: file request size
    alose #29

--- server/src/main/java/org/adp/gable/security/controller/UserController.java
+++ server/src/main/java/org/adp/gable/security/controller/UserController.java
@@ -1,12 +1,17 @@
 package org.adp.gable.security.controller;
 
+import org.adp.gable.common.beans.Result;
+import org.apache.commons.io.FileUtils;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @author zzq
@@ -21,8 +26,12 @@ public class UserController {
     }
 
     @PostMapping
-    private String postFile(MultipartFile uploadFile) throws IOException {
-        System.out.println(new String(uploadFile.getBytes()));
-        return uploadFile == null ? "'empty file'" : uploadFile.getOriginalFilename();
+    private Result<String> postFile(MultipartFile uploadFile) throws IOException {
+        System.out.println("file name: " + uploadFile.getName());
+        System.out.println("file getOriginalFilename: " + uploadFile.getOriginalFilename());
+        final File file = FileUtils.getFile("C:\\app\\nginx\\html\\a\\", uploadFile.getOriginalFilename());
+        FileUtils.copyInputStreamToFile(uploadFile.getInputStream(), file);
+        String name = uploadFile == null ? "'empty file'" : uploadFile.getOriginalFilename();
+        return Result.success(name);
     }
 }
