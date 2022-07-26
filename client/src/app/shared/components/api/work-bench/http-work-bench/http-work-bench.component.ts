dc29e9f466d477594ff9126737748f95b4aeb00d
zzq <1694392889@qq.com>
2022-07-26 23:13:44 +0800

    feat: add data get api

--- client/src/app/shared/components/api/work-bench/http-work-bench/http-work-bench.component.ts
+++ client/src/app/shared/components/api/work-bench/http-work-bench/http-work-bench.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {RequestTabsComponent} from './inner/request-tabs/request-tabs.component';
 
 @Component({
   selector: 'app-http-work-bench',
@@ -6,8 +7,12 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./http-work-bench.component.scss']
 })
 export class HttpWorkBenchComponent implements OnInit {
+  @ViewChild(RequestTabsComponent)
+  req!: RequestTabsComponent;
   @Output() run = new EventEmitter();
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -20,7 +25,9 @@ export class HttpWorkBenchComponent implements OnInit {
   }
 
   onSend() {
-    console.log('zzq see click');
+    const data: any = {};
+    data.req = this.req.getData();
+    console.log('zzq see data wait for send', data);
     // @ts-ignore
     gtag('event', 'run_test', {
       type: 'Http',
--- client/src/app/shared/components/api/work-bench/http-work-bench/inner/request-tabs/request-tabs.component.ts
+++ client/src/app/shared/components/api/work-bench/http-work-bench/inner/request-tabs/request-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {QueryTableComponent} from '../query-table/query-table.component';
 
 @Component({
   selector: 'app-request-tabs',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-tabs.component.scss']
 })
 export class RequestTabsComponent implements OnInit {
+  @ViewChild(QueryTableComponent)
+  query!: QueryTableComponent;
   tabs = ['Params', 'Authorization', 'Header', 'Body', 'Pre-Script', 'Post-Script'];
   curTab = 'Params';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getData(): any {
+    const data: any = {};
+    data.query = this.query.getData();
+    return data;
   }
 
 }
