0a89ed60c0ef676f2c4769a087471930ab44ecc9
BreakZhao <1694392889@qq.com>
2022-07-29 10:24:30 +0800

    feat: body text selector
    about: 37

--- client/src/app/shared/components/api/work-bench/http-work-bench/response-tabs/body-text/body-text.component.ts
+++ client/src/app/shared/components/api/work-bench/http-work-bench/response-tabs/body-text/body-text.component.ts
@@ -6,15 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body-text.component.scss']
 })
 export class BodyTextComponent implements OnInit {
+  bodyType = 'json';
   editorOptions = {
     theme: 'vs-light', fontSize: 12, glance: false, minimap: {enabled: false},
     lineDecorationsWidth: 1, language: 'json'
   };
   code = '{}';
   showType = 'Pretty';
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onBodyTypeChange($event: any): void {
+  }
 }
