8b4c87b13a6a721ffe5186df4014e1f12983b895
BreakZhao <1694392889@qq.com>
2022-08-04 17:08:49 +0800

    feat: add monaco editor content change listener
    about:#41

--- client/src/app/shared/components/api/work-bench/http-work-bench/request-tabs/text-body/text-body.component.ts
+++ client/src/app/shared/components/api/work-bench/http-work-bench/request-tabs/text-body/text-body.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MonacoStandaloneCodeEditor} from '@materia-ui/ngx-monaco-editor';
+import {debounceTime, distinctUntilChanged, Subject} from "rxjs";
 
 @Component({
   selector: 'app-text-body',
   templateUrl: './text-body.component.html',
   styleUrls: ['./text-body.component.scss']
 })
-export class TextBodyComponent implements OnInit {
+export class TextBodyComponent implements OnInit, OnDestroy {
   editorOptions = {
     theme: 'vs-light', fontSize: 12, glance: false, minimap: {enabled: false},
     lineDecorationsWidth: 1, language: 'json'
   };
   code = '{}';
+  editor: MonacoStandaloneCodeEditor;
+  contentSubject = new Subject<void>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.contentSubject.pipe(debounceTime(500))
+      .subscribe(() => {
+      });
   }
 
   setBodyLang(language): void {
     this.editorOptions = {...this.editorOptions, language};
+  }
+
+  initEditor(e: MonacoStandaloneCodeEditor) {
+    this.editor = e;
+    e.onDidChangeModelContent((ey) => {
+      this.contentSubject.next();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.contentSubject.unsubscribe();
   }
 }
