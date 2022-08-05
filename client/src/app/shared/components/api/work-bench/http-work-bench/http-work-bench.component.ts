0dc0ae66546d1104a56250f2f999b94e380ef42d
BreakZhao <1694392889@qq.com>
2022-08-05 14:46:03 +0800

    feat: url && method change listener
    about:#41

--- client/src/app/shared/components/api/work-bench/http-work-bench/http-work-bench.component.ts
+++ client/src/app/shared/components/api/work-bench/http-work-bench/http-work-bench.component.ts
@@ -1,20 +1,23 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {RequestTabsComponent} from './request-tabs/request-tabs.component';
-import {ApiMenuServiceImpl} from "../../../../../core/services/impl/api-menu-impl.service";
-import {ApiMenuCollection, ApiMenuItem} from "../../../../../core/services/entity/ApiMenu";
-import {ApiHeaderOperationComponent} from "../../api-header-operation/api-header-operation.component";
+import {ApiMenuServiceImpl} from '../../../../../core/services/impl/api-menu-impl.service';
+import {ApiMenuItem} from '../../../../../core/services/entity/ApiMenu';
+import {ApiHeaderOperationComponent} from '../../api-header-operation/api-header-operation.component';
+import {debounceTime, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-http-work-bench',
   templateUrl: './http-work-bench.component.html',
   styleUrls: ['./http-work-bench.component.scss']
 })
-export class HttpWorkBenchComponent implements OnInit {
+export class HttpWorkBenchComponent implements OnInit, OnDestroy {
   @ViewChild(RequestTabsComponent)
   req!: RequestTabsComponent;
   @ViewChild(ApiHeaderOperationComponent)
   header!: ApiHeaderOperationComponent;
   @Output() run = new EventEmitter();
+  urlSubject = new Subject<void>();
+  url: string;
 
   constructor(
     private menuService: ApiMenuServiceImpl
@@ -22,6 +25,10 @@ export class HttpWorkBenchComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.urlSubject.pipe(debounceTime(1000))
+      .subscribe(() => {
+        console.log('url changed', this.url);
+      });
   }
 
   setApiData(id: number, isEdit: boolean = false) {
@@ -35,6 +42,7 @@ export class HttpWorkBenchComponent implements OnInit {
   }
 
   methodChange(data: any): void {
+    console.log('zzq see method changed', data);
   }
 
   onSend() {
@@ -49,5 +57,13 @@ export class HttpWorkBenchComponent implements OnInit {
 
   onReqTabChanged(tabKey: string) {
     console.log('see tab changed to be ', tabKey);
+  }
+
+  onUrlChange() {
+    this.urlSubject.next();
+  }
+
+  ngOnDestroy(): void {
+    this.urlSubject.unsubscribe();
   }
 }
